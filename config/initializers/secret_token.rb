3c70b6e2840d4f3169ee897c03ecc843f78f8d81
szk <szk.create@gmail.com>
2015-01-19 18:12:52 +0900

    Initialize Second

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,6 +10,8 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
+require 'securerandom'
+
 def secure_token
   token_file = Rails.root.join('.secret')
   if File.exist?(token_file)
